47204cefc1dbd01cbc1c4e66b4b857737e508ae6
jstone18 <jason.stone0808@gmail.com>
2019-10-04 22:57:03 -0400

    Installl and import redux packages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk from 'react-thunk'
+
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
