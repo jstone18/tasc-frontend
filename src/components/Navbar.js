c9b77802efdf3895fa0ffdbe76b458b042beaa98
jstone18 <jason.stone0808@gmail.com>
2019-10-07 19:31:49 -0400

    Add google fonts, add styling to app

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,13 @@
-import '../css/navbar.css'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div className="ui secondary pointing menu navbar">
-      <Link to='/' className="active item">Dashboard</Link>
-      <Link to='/students' className=" item">Students</Link>
+    <div className="nav-bar">
+      <div className="ui secondary pointing menu navbar">
+        <Link to='/' className="active item">Dashboard</Link>
+        <Link to='/students' className=" item">Students</Link>
+      </div>
     </div>
   )
 }
