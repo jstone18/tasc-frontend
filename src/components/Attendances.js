160d426def598a413a9563a14da22f968eb6b7a4
jstone18 <jason.stone0808@gmail.com>
2019-10-12 10:37:09 -0400

    Create possible rendering of attendance record

--- src/components/Attendances.js
+++ src/components/Attendances.js
@@ -1,9 +1,26 @@
 import React from 'react'
 
 const Attendances = (props) => {
+  
+
   return (
     <div>
-      Attendances
+      {props.attendances.map(attendance => 
+        <table className="ui celled table">
+          <thead>
+            <tr>
+            <th><strong>Date</strong></th>
+            <th><strong>Status</strong></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td data-label="Date">{attendance.created_at}</td>
+              <td data-label="Status">{attendance.check_in ? "Checked-In" : "Checked-Out"}</td>
+            </tr>
+          </tbody>
+        </table>
+      )}
     </div>
   )
 }
