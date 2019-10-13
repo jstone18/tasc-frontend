19252da03a14d647929df50810dd408509bacbee
jstone18 <jason.stone0808@gmail.com>
2019-10-13 02:35:09 -0400

    Modify attendance record on students profile

--- src/components/Attendances.js
+++ src/components/Attendances.js
@@ -2,27 +2,30 @@ import React from 'react'
 
 const Attendances = (props) => {
   
-
-  return (
-    <div>
-      {props.attendances.map(attendance => 
-        <table className="ui celled table">
-          <thead>
-            <tr>
-            <th><strong>Date</strong></th>
-            <th><strong>Status</strong></th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td data-label="Date">{attendance.created_at}</td>
-              <td data-label="Status">{attendance.check_in ? "Checked-In" : "Checked-Out"}</td>
-            </tr>
-          </tbody>
-        </table>
-      )}
-    </div>
-  )
+  if (props.attendances.length === 0) {
+    return <p>There is no Attendance Record.</p>
+  } else {
+    return (
+      <div>
+        {props.attendances.map(attendance => 
+          <table className="ui celled table">
+            <thead>
+              <tr>
+              <th><strong>Date</strong></th>
+              <th><strong>Status</strong></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td data-label="Date">{attendance.created_at}</td>
+                <td data-label="Status">{attendance.check_in ? "Checked-In" : "Checked-Out"}</td>
+              </tr>
+            </tbody>
+          </table>
+        )}
+      </div>
+    )
+  }
 }
 
 export default Attendances
