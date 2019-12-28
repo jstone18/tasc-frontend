c4014762244df3ee0c93c5bb45ba0ebee87f27e6
jstone18 <jason.stone0808@gmail.com>
2019-12-28 17:32:24 -0500

    Removed RoomsHeader component. Create header in list component.

--- src/components/room/RoomsList.js
+++ src/components/room/RoomsList.js
@@ -1,28 +1,27 @@
-import React from 'react'
-import RoomCard from './RoomCard'
+import React from "react";
+import RoomCard from "./RoomCard";
 
 const RoomsList = ({ rooms, students }) => {
+	const room = rooms.map(room => (
+		<RoomCard key={room.id} room={room} students={students} />
+	));
 
-  const room = rooms.map(room => 
-    <RoomCard 
-      key={room.id} 
-      room={room} 
-      students={students}
-      />)
-      
-  return (
-    <div className="ui segment">
-      <table className="ui fixed single line celled table">
-        <thead>
-          <tr>
-            <th className="four wide">Room Titile</th>
-            <th>Room Assignments</th>
-          </tr>
-        </thead>
-        {room}
-      </table>    
-    </div>
-  )
-}
+	return (
+		<div className="ui segment">
+			<div className="list-header">
+				<h2>Rooms List</h2>
+			</div>
+			<table className="ui fixed single line celled table">
+				<thead>
+					<tr>
+						<th className="four wide">Room Titile</th>
+						<th>Room Assignments</th>
+					</tr>
+				</thead>
+				{room}
+			</table>
+		</div>
+	);
+};
 
-export default RoomsList
+export default RoomsList;
--- src/containers/RoomsContainer.js
+++ src/containers/RoomsContainer.js
@@ -1,35 +1,29 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { getRooms } from '../redux/actions/rooms'
-import RoomsList from '../components/room/RoomsList'
-import RoomHeader from '../components/room/RoomHeader'
-import ShowFormButton from '../components/room/RoomFormButtton'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { getRooms } from "../redux/actions/rooms";
+import RoomsList from "../components/room/RoomsList";
+import ShowFormButton from "../components/room/RoomFormButtton";
 
 class RoomsContainer extends Component {
+	componentDidMount() {
+		this.props.getRooms();
+	}
 
-  componentDidMount() {
-    this.props.getRooms()
-  }
-  
-  render() {
-    
-    return (
-      <div>
-        <RoomHeader />
-        <RoomsList rooms={ this.props.rooms } students={ this.props.students }  />
-        <ShowFormButton  />
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div>
+				<RoomsList rooms={this.props.rooms} students={this.props.students} />
+				<ShowFormButton />
+			</div>
+		);
+	}
 }
 
 const mapStateToProps = state => {
-  
-  return {
-    rooms: state.rooms, 
-    students: state.students
-  }
-}
+	return {
+		rooms: state.rooms,
+		students: state.students
+	};
+};
 
-
-export default connect(mapStateToProps, { getRooms })(RoomsContainer)
+export default connect(mapStateToProps, { getRooms })(RoomsContainer);
